311dcf7c9a87216a10a2fc4645b8b44ed8389310
marriammu <marimmuhammed1@gmail.com>
2021-05-06 03:12:51 +0200

    Part-B

--- Part-B/Fourier.cpp
+++ Part-B/Fourier.cpp
@@ -8,24 +8,18 @@
 #include<pybind11/complex.h>
 using namespace std;
 namespace py = pybind11;
-//You can read information from files into your C++ program. This is possible using stream extraction operator (>>). You use the operator in the same way you use it to read user input from the keyboard. However, instead of using the cin object, you use the ifstream/ fstream object.
-// ifstream inFile;
-// // Reading file
-
-// //float x = 0;
-// double x = 0;
 
 
 ///////////////////DFT function
 vector<complex<double>> DFT(vector<complex<double>> data)
 {
     int NumberOfSamples = data.size();
-    int CopyOfNumberOfSampels = NumberOfSamples;
+    int K = NumberOfSamples;
     complex<double> Frequency;
     vector<complex<double>> Frequencies;
-    Frequencies.reserve(CopyOfNumberOfSampels);
+    Frequencies.reserve(K);
 
-    for (int k = 0; k < CopyOfNumberOfSampels; k++)
+    for (int k = 0; k < K; k++)
     {
        Frequency=(0);
 
@@ -46,46 +40,31 @@ vector<complex<double>> DFT(vector<complex<double>> data)
 
 vector<complex<double>> FFT(vector<complex<double>> &sampels)
 {
-    double pi = 3.14159265358979323846;
-    // find the number of samples we have
     int NumberOfSampels = sampels.size();
-    // Executr the end of the recursive even/odd splits once we only have one sample
     if (NumberOfSampels == 1)
     {
         return sampels;
     }
 
-    /* Split the sampels into even and odd subsums */
-    // Find half the total number of samples
     int M = NumberOfSampels / 2;
 
-    // Declare an even and odd complex vector
     vector<complex<double>> SampelsEven(M, 0);
     vector<complex<double>> SampelsOdd(M, 0);
 
-    // Input the even and odd sampels into recursive vectors
     for (int i = 0; i != M; i++)
     {
         SampelsEven[i] = sampels[2 * i];
         SampelsOdd[i] = sampels[2 * i + 1];
     }
-    // Perform the recursive FFT operation on the odd and even sides
     vector<complex<double>> FrequenciesEven(M, 0);
     FrequenciesEven = FFT(SampelsEven);
     vector<complex<double>> FrequenciesOdd(M, 0);
     FrequenciesOdd = FFT(SampelsOdd);
-    /*------ END RECURSION ______ */
-
-    // Declare vector of frequency bins
     vector<complex<double>> Frequencies(NumberOfSampels, 0);
-    // Combine the values found
     for (int k = 0; k != NumberOfSampels / 2; k++)
     {
-        // For each split set, we need to multiply a k-dependent comlpex
-        //number by the odd sunsum
-        complex<double> ComplexExponential = polar(1.0, -2 * pi * k / NumberOfSampels) * FrequenciesOdd[k];
+        complex<double> ComplexExponential = polar(1.0, -2 * M_PI * k / NumberOfSampels) * FrequenciesOdd[k];
         Frequencies[k] = FrequenciesEven[k] + ComplexExponential;
-        // Everytime you add pi, exponential changes sign
         Frequencies[k + NumberOfSampels / 2] = FrequenciesEven[k] - ComplexExponential;
     }
     return Frequencies;
